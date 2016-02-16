7856a873978807aa5c93d1b9108706c274f25030
AustinCullar <austincullar@yahoo.com>
2016-02-16 00:32:08 -0600

    adding image test

---
+++ OilAnalysis.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 00:16:41 2016
+
+@author: AustinC
+"""
+
+
+import Image
+print("hello")  
+jpgfile = Image.open("Image.jpg")
+
+#print (jpgfile.bits, jpgfile.size, jpgfile.format)
