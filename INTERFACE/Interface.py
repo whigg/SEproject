a929c07f780921bc9ec2ff79a1004175630181a6
KAndrew340 <Kem_340@hotmail.com>
2016-03-02 21:06:29 -0600

    adding interface folder

---
+++ INTERFACE/Interface.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 02 16:10:15 2016
+
+@author: Kem
+"""
+
+#INTERFACE
+#---------
+
+import sys
+from PyQt4 import QtGui, QtCore
+
+class Window(QtGui.QMainWindow):
+    
+    def __init__(self):
+        #Template for home menu
+        super(Window,self).__init__()
+        self.setGeometry(50,50,500,300)
+        self.setWindowTitle("PIXELS")
+        self.setWindowIcon(QtGui.QIcon('logo2'))
+        self.home()
+        
+    def home(self):
+        button = QtGui.QPushButton("Browse", self)
+        button.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        
+        self.show()
+        
+  
+def run():
+    app= QtGui.QApplication(sys.argv)
+    GUI = Window()
+    sys.exit(app.exec_())
+      
+run()
