56163510ad7e2218d62beac10b3597a62d556b25
AustinCullar <austincullar@yahoo.com>
2016-03-02 20:33:28 -0600

    all images in a folder are being processed, with the results being saved in the same directory; working on a way to create a seperate folder for results

--- OilAnalysis.py
+++ OilAnalysis.py
@@ -15,26 +15,38 @@ Dr. Colmaneres
 #script, converts it to binary, and saves the result in the same directory
 #with the name "result".
 
+##  NOTE  ##
+#Depending on how the GUI ends up looking, we could offer a text box for the
+#user to specify the folder name in which all of the images he/she is providing
+#will be stored. This would allow us to avoid the hard-coding of a particular
+#folder name. (Such as 'Images', as I have done below.)
+
 from PIL import Image
 import os
 
+#list to hold all binary images
 binary_images = []
 
 #getting names of all files in the Images directory
 filenames = os.listdir('Images')
 print(filenames)
-
 #changing directory to 'Images" folder
 os.chdir('Images')
 
-image_file = Image.open("k3.jpg") #open color image
+for i in range(len(filenames)):
+    #open color image
+    image_file = Image.open(filenames[i]) 
 
-image_file = image_file.convert('1') #convert to black and white
+    #convert to black and white
+    image_file = image_file.convert('1') 
 
-binary_images.append(image_file)
-print(binary_images)
+    #adding new binary image to binary_images list
+    binary_images.append(image_file)
+
+for i in range(len(binary_images)):
+    image_file = binary_images[i]
+    image_file.save('result' + str(i) + '.png')
 #image_file.save('result.png')
-
 #######################################################################
 
 #attempting to read all file names in the images directory
