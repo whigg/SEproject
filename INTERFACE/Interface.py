4c505b090f6c68c64e9a0a4d45cc0dc44255a931
KAndrew340 <Kem_340@hotmail.com>
2016-03-06 08:27:18 -0600

    added functionality to inteface: tool-tip, messagebox

--- INTERFACE/Interface.py
+++ INTERFACE/Interface.py
@@ -16,16 +16,62 @@ class Window(QtGui.QMainWindow):
     def __init__(self):
         #Template for home menu
         super(Window,self).__init__()
-        self.setGeometry(50,50,500,300)
+        #self.resize(250, 150) optional resize method
+        self.setGeometry(400,100,500,500)
         self.setWindowTitle("PIXELS")
-        self.setWindowIcon(QtGui.QIcon('Image'))
+        self.setWindowIcon(QtGui.QIcon('icon'))
+
+
+        # extractAction = QtGui.QAction("Get to the choppah", self)
+        # extractAction.setShortcut("Ctrl+Q")
+        # extractAction.setStatusTip('Leave the app')
+        # extractAction.triggered.connect(QtCore.QCoreApplication.instance().quit)
+        #
+        # self.stausBar()
+        #
+        # mainMenu = self.menuBar()
+        # fileMenu = mainMenu.addMenu('&File')
+        # fileMenu.addAction(extractAction)
+
         self.home()
 
     def home(self):
-        button = QtGui.QPushButton("Browse", self)
-        button.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        #Call the function to center the window
+        self.center()
 
+        #This is a tooltip
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+        self.setToolTip('Thanks for using <b>P I X E L S</b>!')
+
+
+        button = QtGui.QPushButton("Exit", self)
+        button.setToolTip('click to <b>EXIT</b> pixels')
+        button.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        button.resize(button.sizeHint())
+        button.move(150,250)
         self.show()
+
+    #This function displays a message box when the user
+    #wants to quit the application
+    def closeEvent(self, event):
+
+        reply = QtGui.QMessageBox.question(self, 'Message',
+            "Are you sure to quit?", QtGui.QMessageBox.Yes |
+            QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    #Centers the window on the screen
+    def center(self):
+
+        qr = self.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
 
 
 def run():
