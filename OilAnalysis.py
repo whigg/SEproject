45a4bc14e650131ac6c80e3d1b82813d554606ce
AustinCullar <austincullar@yahoo.com>
2016-03-26 14:07:42 -0500

    imageData[] now contains information about all images, images folder changed to test folder containing one image, continuing with graph creation

--- OilAnalysis.py
+++ OilAnalysis.py
@@ -7,8 +7,8 @@ Oil Dispersal Analysis
 Dr. Colmaneres
 """
 
-#The following section of code opens a folder of image files in the directory 
-#of this python script, converts it to binary, and saves the results in another 
+#The following section of code opens a folder of image files in the directory
+#of this python script, converts it to binary, and saves the results in another
 #directory.
 
 ##  NOTE  ##
@@ -21,12 +21,12 @@ Dr. Colmaneres
 from PIL import Image
 import os
 
-
 #name for folder in which all resulting binary images will be saved.
 result_folder = 'BinaryImages'
 
 #name for folder in which all original images will be kept.
-images_folder = 'Images'
+#images_folder = 'Images'
+images_folder = 'TestImage'
 
 #list to hold all binary images
 binary_images = []
@@ -37,6 +37,11 @@ binaryImageNames = []
 #list to hold touples which hold data for each image
 imageData = []
 
+#variable to hold number of black pixels
+Bpixels = 0
+
+#variable to hold number of white pixels
+Wpixels = 0
 
 #getting names of all files in the Images directory
 filenames = os.listdir(images_folder)
@@ -46,7 +51,7 @@ os.chdir(images_folder)
 
 for i in range(len(filenames)):
     #open color image
-    image_file = Image.open(filenames[i]) 
+    image_file = Image.open(filenames[i])
 
     #convert to pure black and white (next two lines)
     image_file = image_file.convert('L')
@@ -70,25 +75,31 @@ for i in range(len(binary_images)):
     image_file = binary_images[i]
     #image_file.save('result' + str(i) + '.png')
     image_file.save(filenames[i] + 'BINARY.png')
-    
+
 os.chdir('..')
 os.chdir('BinaryImages')
 binaryImageNames = os.listdir(os.getcwd())
-    
+
 for i in range(len(binaryImageNames)):
-    
+
     im = Image.open(binaryImageNames[i])
     print(binaryImageNames[i])
 
     PixList = im.getcolors()
 
     for pixel in PixList:
+
         if pixel[1] == 0:
+            Bpixels = pixel[0]
             print ("Black: ", pixel[0])
-        
-        elif pixel[1] == 255:     
+
+        elif pixel[1] == 255:
+            Wpixels = pixel[0]
             print ("White: ", pixel[0])
-    
+
         else:
             print ("Non B/W Pixel detected!!")
 
+    imageData.append((binaryImageNames[i], Bpixels, Wpixels))
+###  At this point, imageData contains information about all images in the
+###  specified images file. Now comes the graph construction.
