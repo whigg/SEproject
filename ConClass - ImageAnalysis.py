94f7752a3697e0d278dc91e2546c7d20d63d53b2
Taylor Murphy <Taylor Murphy>
2016-04-09 19:52:45 -0500

    controller updates

--- ConClass - ImageAnalysis.py
+++ ConClass - ImageAnalysis.py
@@ -1,5 +1,12 @@
 ###############################################
 # Name: Taylor Murphy
-#Image Analysis Controller Class
+# Image Analysis Controller Class
 ###############################################
-#
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
--- OilAnalysis.py
+++ OilAnalysis.py
@@ -24,93 +24,53 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 
-#name for folder in which all resulting binary images will be saved.
-result_folder = 'BinaryImages'
+result_folder = 'BinaryImages'  #name for folder in which all resulting binary images will be saved.
+images_folder = 'OilFilled'     #name for folder in which all original images will be kept.
+binary_images = []              #list to hold all binary images
+binaryImageNames = []           #list to hold all file names in the binary images folder
+imageData = []                  #list to hold touples which hold data for each image
+Bpixels = 0                     #variable to hold number of black pixels
+Wpixels = 0                     #variable to hold number of white pixels
+size = 128, 128
 
-#name for folder in which all original images will be kept.
-images_folder = 'OilFilled'
+filenames = os.listdir(images_folder)               #getting names of all files in the Images directory
 
-#list to hold all binary images
-binary_images = []
-
-#list to hold all file names in the binary images folder
-binaryImageNames = []
-
-#list to hold touples which hold data for each image
-imageData = []
-
-#variable to hold number of black pixels
-Bpixels = 0
-
-#variable to hold number of white pixels
-Wpixels = 0
-
-#getting names of all files in the Images directory
-filenames = os.listdir(images_folder)
-
-#changing directory to 'Images" folder
-os.chdir(images_folder)
+os.chdir(images_folder)                             #changing directory to 'Images" folder
 
 for i in range(len(filenames)):
-    #open color image
-    image_file = Image.open(filenames[i])
+    image_file = Image.open(filenames[i])           #open color image
 
-    #convert to pure black and white (next two lines)
-    image_file = image_file.convert('L')
+    
+    image_file = image_file.convert('L')            #convert to pure black and white (next two lines)
     image_file = image_file.point(lambda x: 0 if x < 128 else 255, '1')
 
-    #adding new binary image to binary_images list
-    binary_images.append(image_file)
+    binary_images.append(image_file)                #adding new binary image to binary_images list
 
-#backing out of images_folder
-os.chdir('..')
 
-#if the result_folder does not exist, create it
-if not os.path.exists(result_folder):
+os.chdir('..')                                      #backing out of images_folder
+
+if not os.path.exists(result_folder):               #if the result_folder does not exist, create it
     os.makedirs(result_folder)
 
-#change directories to result folder
-os.chdir(result_folder)
+os.chdir(result_folder)                             #change directories to result folder
 
-#save all binary images in the result_folder
-for i in range(len(binary_images)):
+for i in range(len(binary_images)):                 #save all binary images in the result_folder
     
     image_file = binary_images[i]
     #image_file.save('result' + str(i) + '.png')
     image_file.save(filenames[i] + 'BINARY.png')
 
-os.chdir('..')
-os.chdir('BinaryImages')
+os.chdir('..')                                      #backing out of result_folder
+os.chdir('BinaryImages')                            #change directories to BinaryImages
 
 binaryImageNames = os.listdir(os.getcwd())
-#print(binaryImageNames)
-
-size = 128, 128
+#print('\n'.join(binaryImageNames))
 
 for infile in glob.glob("*.jpg"):
     file, ext = os.path.splitext(infile)
     im = Image.open(binaryImageNames[i])
     im.thumbnail(size, Image.ANTIALIAS)
     im.save(file + ".thumbnail", "JPEG")
-
-# for i in range(len(binaryImageNames)):
-#
-#     im = Image.open(binaryImageNames[i])
-#     print(binaryImageNames[i])
-# 
-#     PixList = im.getcolors()
-#
-#     for pixel in PixList:
-#         if pixel[1] == 0:
-#             print ("Black: ", pixel[0])
-#
-#         elif pixel[1] == 255:
-#             print ("White: ", pixel[0])
-#
-#         else:
-#             print ("Non B/W Pixel detected!!")
-
-binaryImageNames = os.listdir(os.getcwd())
 
 for i in range(len(binaryImageNames)):
 
@@ -138,14 +98,14 @@ for i in range(len(binaryImageNames)):
 
 
 #x coordinate will be the positions in sequence of each image
-x = range(0,7)
+#x = range(0,7)
 
 #y coordinate will be the number of black pixels
-y = [i[1] for i in imageData]
-print(y)
+#y = [i[1] for i in imageData]
+#print(y)
 
-plt.plot(x, y)
+#plt.plot(x, y)
 #plt.axis([0, 25, 800000, 1400000])
-plt.xlabel("Position in Sequence")
-plt.ylabel("Pixels")
-plt.show()
+#plt.xlabel("Position in Sequence")
+#plt.ylabel("Pixels")
+#plt.show()
