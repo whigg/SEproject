1ed65aa53bfce149d7d716c33dd8c2fa7cde6c79
Taylor Murphy <Taylor Murphy>
2016-05-01 01:56:51 -0500

    Fixed Auto Threshholding

--- AutoThresholdingTester.py
+++ AutoThresholdingTester.py
@@ -2,12 +2,17 @@
 # Name: Taylor Murphy
 # AutoThresholding Tester
 ###############################################
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
 #Start AutoThresholding Tester ################################################
+imgRaw = cv2.imread('DI evapo 2fps-000000.png', 0)
+imgRaw = cv2.GaussianBlur(imgRaw , (9, 9), -10)
 
+imgBin = cv2.adaptiveThreshold(imgRaw, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 1)
 
+cv2.imwrite('DI evapo 2fps-000000BIN.png', imgBin)
 
 
 #End AutoThresholding Tester ##################################################
-
-    
--- ConClass - ImageAnalysis.py
+++ ConClass - ImageAnalysis.py
@@ -4,6 +4,7 @@
 ###############################################
 from PIL import Image
 import os
+import cv2
 
 #Start IAController ###########################################################
 class IAController:
@@ -20,7 +21,7 @@ class IAController:
     
     def __init__(self):                                             #This constructor will be overloaded to allow the GUI to pass directories
         #print("Initialized!")
-        self.rawDataDir = "5-13 m4-3 300nm ipa evapo"
+        self.rawDataDir = "OilFilled"
         self.binDataDir = "BINARIES"
         
         if not os.path.exists(self.binDataDir):                     #if binDataDir does not exist, create it
@@ -33,9 +34,9 @@ class IAController:
         os.chdir(self.rawDataDir)                                   #changing directory to rawDataDir
 
         for i in range(len(self.rawImageNames)):
-            self.rawDataDir = Image.open(self.rawImageNames[i])     #open raw color image
-            self.rawDataDir = self.rawDataDir.convert('L')          #convert to pure black and white (next two lines)
-            self.rawDataDir = self.rawDataDir.point(lambda x: 0 if x < 128 else 255, '1')
+            self.rawDataDir = cv2.imread(self.rawImageNames[i], 0)
+            self.rawDataDir = cv2.GaussianBlur(self.rawDataDir , (9, 9), -10)
+            self.rawDataDir = cv2.adaptiveThreshold(self.rawDataDir, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 1)
             self.binImages.append(self.rawDataDir)                  #adding new binary image to binImages list
 
         os.chdir('..')                                              #backing out of rawDataDir
@@ -43,13 +44,13 @@ class IAController:
 
         for i in range(len(self.binImages)):                        #save all binary images in binDataDir
             self.rawDataDir = self.binImages[i]
-            self.rawDataDir.save(self.rawImageNames[i] + ' BIN.png')
-
+            cv2.imwrite(self.rawImageNames[i] + ' BIN.png', self.rawDataDir)            
+            
         os.chdir('..')                                              #backing out of binDataDir
                 
     def PixelCounter(self):
         self.binImageNames = os.listdir(self.binDataDir)        
-        os.chdir(self.binDataDir)                                   #change directorie to binDataDir
+        os.chdir(self.binDataDir)                                   #change directories to binDataDir
        
         Bpixels = 0                                                 #integer to hold number of black pixels
         Wpixels = 0                                                 #integer to hold number of white pixels        
@@ -76,9 +77,6 @@ class IAController:
                     print ("Non B/W Pixel detected!!")
                     
         os.chdir('..')                                              #backing out of binDataDir
-    
-    def AutoThreshold(self):
-        return 0
         
     def Stitcher(self):
         return 0        
@@ -92,7 +90,6 @@ class IAController:
 experiment = IAController()
 experiment.ImageConverter()
 experiment.PixelCounter()
-experiment.AutoThreshold()
 experiment.Stitcher()
 
 
