6e703c9a35ff880ec2ed84274c1c577787aa000e
AustinCullar <austincullar@yahoo.com>
2016-05-01 20:04:08 -0500

    still working on stitching method; searching panoramic algorithms now

--- ImageStitchingTest.py
+++ ImageStitchingTest.py
@@ -22,12 +22,39 @@ img2 = Image.open(filenames[1])
 pixels1 = img1.load() #ceating a pixel map for first Image
 pixels2 = img2.load() #creating a pixel map for second Image
 
-print(img1.size[1]) #rows
-print(img1.size[0])# columns
+print(img2.size[1]) #rows
+print(img2.size[0]) #columns
 
-for i in range(img1.size[1]):
-    for j in range(img1.size[0]):
-        for k in range(img2.size[0]):
+rows = img1.size[1]
+img1cols = img1.size[0]
+
+img2cols = img2.size[0]
+#no img2row variable since the number of rows will be the same for both images
+
+#pixel[col, row]
+
+count = 0
+
+if(img1cols > img2cols):
+    cols = img2cols #use the image with the least number of columns for indexing
+        #this shouldnt be an issue if all of the images are of the same size
+else:
+    cols = img1cols #catch case in case the images are of the same size
+
+match = True
+found = False
+
+while(match!=True):
+    for j in range(0, cols): #columns
+        for i in range(0, rows): #rows
+            if (pixels1[(img1cols-1) -j, i] != pixels2[j, i]):
+                match = False
+                break
+        if (match!= True):
+            break
+
+
+print(count)
 
 
 
